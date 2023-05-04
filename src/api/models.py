99699e1eabd1ab9e3c1d01351e6d4f42d0da6ffe
CL4UD3PT <98550012+CL4UD3PT@users.noreply.github.com>
2023-05-04 21:36:47 +0000

    all methods requested working

--- src/api/models.py
+++ src/api/models.py
@@ -7,6 +7,7 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    todos = db.relationship('Todo', backref='user')
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -17,3 +18,16 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    label = db.Column(db.String(120), unique=False, nullable=False)
+    done = db.Column(db.Boolean(), unique=False, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), unique=False, nullable=False)
+
+    def serialize(self):
+        return {
+            "done": self.done,
+            "label": self.label
+        }
+
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,17 +2,38 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Todo
 from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
 
+@api.route('/todos', methods=['GET'])
+def get_all_todos():
+    todos = Todo.query.all()
+    todos_serialized = [todo.serialize() for todo in todos]
+    return jsonify({"todos": todos_serialized}), 200
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
-
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
-
+@api.route('/todo', methods=['POST'])
+def create_todo():
+    body = request.json
+    new_todo = Todo(done=body['done'], label=body['label'], user_id=body['user_id'])
+    db.session.add(new_todo)
+    db.session.commit()
+    response_body = {"Todos": "Todo created successfully"}
     return jsonify(response_body), 200
+
+@api.route('/todo/<int:todo_id>', methods=['GET'])
+def get_one_todo(todo_id):
+    todo = Todo.query.get(todo_id)
+    if not todo:
+        return jsonify({"erro": "Not able to find Todo with the provided id."}), 400
+    return jsonify({'todo': todo.serialize()}), 200
+
+@api.route('/todo/<int:todo_id>', methods=['DELETE'])
+def delete_todo(todo_id):
+    todo = Todo.query.get(todo_id)
+    if not todo:
+        return jsonify({"error": "Not todo found with this id"}), 400
+    db.session.delete(todo)
+    db.session.commit()
+    return jsonify({"todo": "Deleted"}), 200
